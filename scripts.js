8ddd0ce042385331388923654d3d83c514a79b12
svvdevell <svvdevel@gmail.com>
2022-09-12 18:40:52 +0300

    added task

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
 "use strict"
+
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("done"), 3000);
+    setTimeout(() => reject(Error), 3000);
+});
+
+console.log(promise);
+promise
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+    .finally(() => console.log("Promise was finished!"));
+
+
+const getResultPromise = async () => {
+    try {
+        const result = await promise;
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        console.log("Promise was finished!");
+    }
+};
+getResultPromise();
+
